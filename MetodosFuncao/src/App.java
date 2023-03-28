603d59aaa942458b17f36bb8f68e59c811f961ce
Marcos Alves <marcosalvesjr007@gmail.com>
2023-03-28 01:26:18 +0000

    Finalização

---
+++ MetodosFuncao/src/App.java
@@ -0,0 +1,6 @@
+public class App {
+    public static void main(String[] args) {
+        calculadora calc = new calculadora();
+        calc.soma(1, 3);
+    }
+}
---
+++ MetodosFuncao/src/calculadora.java
@@ -0,0 +1,6 @@
+public class calculadora {
+    public int soma(int a, int b){
+        int resultado = a * b;
+        return resultado;
+    }
+}
